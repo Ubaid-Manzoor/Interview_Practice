d7632aa9cc618ac44e452340cccc01bacc813dab
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-07-01 17:27:42 +0530

    Find maximum value of Sum( i*arr[i]) with only rotations on given array allowed

---
+++ Data Structures Problems/Array Problems/max_sum_with_only_rotation/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+// We have to Max Sum(i * arr[i]) with only rotaions on given array allowed
+
+int max_rotation_sum(vector<int> arr){
+    int arr_sum = accumulate(arr.begin(),arr.end(),0);
+
+    int indexSum = 0;
+    for(int i = 1 ; i < arr.size(); i++)
+        indexSum += i * arr[i];
+
+    int max_sum = indexSum;
+    for(int i = 0,right=arr.size()-1 ; i < arr.size() - 1 ; i++,right--){
+        indexSum = indexSum + arr_sum - (arr.size()) * right;
+        max_sum = max(max_sum,indexSum);
+    }
+
+    return max_sum;
+}
+
+int main()
+{
+    vector<int> arr = {10, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    cout<<max_rotation_sum(arr)<<endl;
+}
