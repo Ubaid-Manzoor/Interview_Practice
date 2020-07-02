b9eb09d3842c79e8631e3ceb4a2ec97c241a8095
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-07-02 20:25:02 +0530

    problem to move zeros to end of array

--- Data Structures Problems/Array Problems/move_zeros_to_end/main.cpp
+++ Data Structures Problems/Array Problems/move_zeros_to_end/main.cpp
@@ -24,15 +24,29 @@ void moveZerosToEnd(vector<int> &arr){
     }
 }
 
+void moveZerosToEnd_I(vector<int> &arr){
+    int count=0;
+    for(int i=0; i < arr.size();i++){
+        if(arr[i] != 0){
+            arr[count] = arr[i];
+            count++;
+        }
+    }
+
+    for(int i = count ; i < arr.size() ; i++)
+        arr[i] = 0;
+}
+
+
 int main()
 {
-//    vector<int> arr={1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
+    vector<int> arr={1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9};
 //    vector<int> arr={1, 2, 0, 4, 3, 0, 5, 0};
 //    vector<int> arr={1, 2, 0, 0, 0, 3, 6};
 //    vector<int> arr={0,0,0,0};
 //    vector<int> arr={1,2,3,4,0,0,0,0};
 //    vector<int> arr={0,0,0,0,1,2,3,4};
-    vector<int> arr={0,0,0,0,1,2,3,4,0,0,0};
-    moveZerosToEnd(arr);
+//    vector<int> arr={0,0,0,0,1,2,3,4,0,0,0};
+    moveZerosToEnd_I(arr);
     printVec(arr);
 }
