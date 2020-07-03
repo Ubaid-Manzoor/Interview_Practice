627a1db1b2dd03b1819ff7d08128e2c6b1724b61
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-07-03 20:18:51 +0530

    problem to find sum of non overlapping subarrays

--- Data Structures Problems/Array Practice/Easy/sum_of_lengths_of_non_overlapping_subarrays/main.cpp
+++ Data Structures Problems/Array Practice/Easy/sum_of_lengths_of_non_overlapping_subarrays/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+// Problem Link :: https://practice.geeksforgeeks.org/problems/sum-of-lengths-of-non-overlapping-subarrays/0
+
+int sum_of_subarrays(vector<int> &arr, int k){
+    int total_count = 0;
+
+    int local_count = 0;
+    bool already_seen_k =false;
+    for(int i = 0 ; i < arr.size() ; i++){
+        if(arr[i] < k)
+            local_count+=1;
+        else if(arr[i] == k){
+            already_seen_k = true;
+            local_count +=1;
+        }
+        else if(already_seen_k){
+            total_count += local_count;
+            already_seen_k = false;
+        }
+        else
+            local_count = 0;
+    }
+
+    if(already_seen_k and local_count != 0)
+        return total_count + local_count;
+    return total_count;
+}
+
+int main(){
+    int testcase;
+    cin>>testcase;
+
+    for(int tc = 0 ; tc < testcase ; tc++ ){
+        int N;cin>>N;
+        vector<int> arr(N);
+        for(int i = 0 ; i < arr.size() ; i++){
+            int value;cin>>value;
+            arr[i] = value;
+        }
+        int k;cin>>k;
+        cout<<sum_of_subarrays(arr,k)<<endl;
+    }
 }
