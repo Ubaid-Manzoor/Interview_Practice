d7b89ece06728aa0c433ecc985d257c4a7a39836
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-07-05 15:25:14 +0530

    equilibrium index of an array

--- Data Structures Problems/Array Practice/Easy/remove_all_duplicated_from_string/main.cpp
+++ Data Structures Problems/Array Practice/Easy/remove_all_duplicated_from_string/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include <unordered_set>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+string remove_duplicates(string str){
+    string new_string;
+    unordered_set<char> char_set;
+    for(int i = 0 ; i < str.size() ; i++){
+        if(char_set.find(str[i]) == char_set.end()){
+            new_string += str[i];
+            char_set.emplace(str[i]);
+        }
+    }
+    return new_string;
+}
+
+int main(){
+    string str = "geeksforgeeks";
+    cout<<remove_duplicates(str)<<endl;
 }
