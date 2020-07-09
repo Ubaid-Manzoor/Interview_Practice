6e699ea53d732a245402b87eebe025c3d9158ac9
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-07-09 13:43:30 +0530

    occurence of an interger in the linked list

---
+++ Data Structures Problems/LinkedList Pratice/Easy/Occurence_of_an_integer_in_a_linked_list/main.cpp
@@ -0,0 +1,106 @@
+// { Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+int count(struct node* head, int search_for);
+/* Link list node */
+struct node
+{
+    int data;
+    struct node* next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}*head;
+
+void insert()
+{
+    int n,i,value;
+    struct node *temp;
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&value);
+        if(i==0)
+        {
+            head=new node(value);
+            head->next=NULL;
+            temp=head;
+            continue;
+        }
+        else
+        {
+            temp->next= new node(value);
+            temp=temp->next;
+            temp->next=NULL;
+        }
+    }
+}
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+int main(void)
+{
+    /* Start with the empty list */
+
+    int t,k,n,value;
+     /* Created Linked list is 1->2->3->4->5->6->7->8->9 */
+     scanf("%d",&t);
+     while(t--)
+     {
+     insert();
+     scanf("%d",&k);
+     value=count(head, k);
+     printf("%d\n",value);
+     }
+     return(0);
+}// } Driver Code Ends
+
+
+/*
+  Return the no. of times search_for value is there in a linked list.
+  The input list will have at least one element
+
+  Node is defined as
+struct node
+{
+    int data;
+    struct node* next;
+
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+}*head;
+*/
+
+int count(struct node* head, int search_for){
+    int count=0;
+    node *current = head;
+    while(current != NULL){
+        if(current->data == search_for)
+            count++;
+        current = current->next;
+    }
+    return count;
+}
+
+
+
+
+
