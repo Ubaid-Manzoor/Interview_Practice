3364912f3f88159d4be6a9f50a344f3447c70a22
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-07-14 16:56:07 +0530

    implementing two stack using one array

--- Data Structures Problems/Stack/Easy/implement_two_stacks_in_array/main.cpp
+++ Data Structures Problems/Stack/Easy/implement_two_stacks_in_array/main.cpp
@@ -80,28 +80,36 @@ public:
 // ****************************************************************************************************
 
 
-/* The method push to push element into the stack 2 */
-void twoStacks :: push1(int x)
-{
+/* The method push to push element into the stack 1 */
+void twoStacks :: push1(int x){
+    if(top2 - top1 <= 1)
+        return;
 
+    arr[++top1] = x;
 }
 
 /* The method push to push element into the stack 2*/
-void twoStacks ::push2(int x)
-{
+void twoStacks ::push2(int x){
+    if(top2 - top1 <= 1)
+        return;
 
+    arr[--top2] = x;
 }
 
 /* The method pop to pop element from the stack 1 */
 //Return the popped element
-int twoStacks ::pop1()
-{
-
+int twoStacks ::pop1(){
+    if(top1 == -1)
+        return -1;
+    int value = arr[top1--];
+    return value;
 }
 
 /* The method pop to pop element from the stack 2 */
 //Return the popped element
-int twoStacks :: pop2()
-{
-
+int twoStacks :: pop2(){
+    if(top2 == size)
+        return -1;
+    int value = arr[top2++];
+    return value;
 }
