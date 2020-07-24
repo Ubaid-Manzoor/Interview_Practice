a751aaa57c0e9120a0eacb3547fb54719307b46b
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-07-24 17:57:48 +0530

    implementation of queue using linked list

---
+++ Data Structures Problems/Queue/Basic/implement_queue_using_linked_list/main.cpp
@@ -0,0 +1,106 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+/* The method push to push element into the queue*/
+void MyQueue:: push(int x)
+{
+    // Your Code
+    QueueNode *node = new QueueNode(x);
+
+    if(rear == NULL or front == NULL){
+        rear = node;
+        front = node;
+    }else{
+        rear->next = node;
+        rear = node;
+    }
+}
+
+/*The method pop which return the element
+  poped out of the queue*/
+int MyQueue :: pop()
+{
+    // Your Code
+    if(front == NULL)
+        return -1;
+
+    int first = front->data;
+
+    QueueNode *temp = front;
+    front = front->next;
+    free(temp);
+
+    return first;
+}
