e7ee93d48d87cd9fe61ae26a911dd861b935f7f0
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-08-05 16:58:02 +0530

    level order Traversal in spiral form

---
+++ Data Structures Problems/TREE/Easy/level_order_traversal_in_spiral_form/main.cpp
@@ -0,0 +1,197 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+void printSpiral(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+int main() {
+    int t;
+    string  tc;
+    getline(cin,tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+
+        printSpiral(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
+
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+void printSpiral(Node *root)
+{
+    if(root == NULL)
+        return;
+    //Your code here
+    stack<Node*> s1;
+    stack<Node*> s2;
+
+
+    s2.emplace(root);
+
+    while(not s1.empty() or not s2.empty()){
+        while(not s2.empty()){// USE RIGHT
+            Node * top = s2.top();
+
+            cout<<top->data<<" ";
+            if(top->right)
+                s1.emplace(top->right);
+            if(top->left)
+                s1.emplace(top->left);
+
+            s2.pop();
+        }
+        while(not s1.empty()){// USE LEFT
+            Node *top = s1.top();
+            cout<<top->data<<" ";
+
+            if(top->left)
+                s2.emplace(top->left);
+            if(top->right)
+                s2.emplace(top->right);
+
+            s1.pop();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
