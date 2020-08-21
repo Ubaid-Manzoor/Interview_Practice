23d6cad78c2f2547b7d4bc1dd03a41b44416a2aa
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-08-21 18:34:05 +0530

    merge two BSTs

--- Data Structures Problems/TREE/Medium/merge_two_BSTs/main.cpp
+++ Data Structures Problems/TREE/Medium/merge_two_BSTs/main.cpp
@@ -126,7 +126,76 @@ struct Node {
 };*/
 
 // Return a integer of integers having all the nodes in both the BSTs in a sorted order.
-vector<int> merge(Node *root1, Node *root2)
-{
-   //Your code here
+vector<int> merge(Node *root1, Node *root2){
+   vector<int> merged_bst;
+
+   stack<Node*> s1,s2;
+   s1.emplace(new Node(INT_MAX));
+   s2.emplace(new Node(INT_MAX));
+   Node* current1 = root1;
+   Node* current2 = root2;
+   bool traverse1=true,traverse2=true;
+
+   while(current1 or current2 or (not s1.empty() and not s2.empty()) ){
+       while(traverse1 and current1 != NULL){
+           s1.emplace(current1);
+           current1 = current1->left;
+       }
+
+       while(traverse2 and current2 != NULL){
+           s2.emplace(current2);
+           current2 = current2->left;
+       }
+
+       if(s1.top()->data <= s2.top()->data){
+           merged_bst.emplace_back(s1.top()->data);
+           current1 = s1.top()->right;
+           s1.pop();
+           traverse1=true;traverse2=false;
+       }else{
+           merged_bst.emplace_back(s2.top()->data);
+           current2 = s2.top()->right;
+           s2.pop();
+           traverse1=false;traverse2=true;
+       }
+   }
+
+
+   merged_bst.pop_back();
+   return merged_bst;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
