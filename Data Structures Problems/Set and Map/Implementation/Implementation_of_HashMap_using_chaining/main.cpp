c9456834f4ca92231978f1e5c3f9600adc98db4f
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-09-15 17:18:41 +0530

    implementation of Hash Table using chaining

---
+++ Data Structures Problems/Set and Map/Implementation/Implementation_of_HashMap_using_chaining/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+class HashMap {
+private:
+    int BUCKET;
+    list<int> *table;
+
+public:
+
+    HashMap(int size);
+    int hashFunction(int key);
+    void insert(int x);
+    void deleteX(int x);
+    void displayHash();
+
+};
+
+HashMap::HashMap(int size){
+    this->BUCKET = size;
+    table = new list<int>[this->BUCKET];
+}
+
+int HashMap::hashFunction(int key){
+    return key % this->BUCKET;
+}
+
+void HashMap::insert(int x){
+    int index = hashFunction(x);
+
+    table[index].emplace_back(x);
+}
+
+void HashMap::deleteX(int x){
+    int index = hashFunction(x);
+
+    table[index].remove(x);
+}
+
+void HashMap::displayHash(){
+    for(int i = 0 ;i < this->BUCKET ; i++){
+        cout<<i<<" ";
+        for(int x: table[i])
+            cout<<x<<"-->";
+        cout<<endl;
+    }
+}
+
+// Driver program
+int main()
+{
+  // array that contains keys to be mapped
+  int a[] = {15, 11, 27, 8, 12};
+  int n = sizeof(a)/sizeof(a[0]);
+
+  // insert the keys into the hash table
+  HashMap h(7);   // 7 is count of buckets in
+               // hash table
+  for (int i = 0; i < n; i++)
+    h.insert(a[i]);
+
+  h.displayHash();
+  // delete 12 from hash table
+  h.deleteX(8);
+
+  // display the Hash table
+  h.displayHash();
+
+  return 0;
+}
