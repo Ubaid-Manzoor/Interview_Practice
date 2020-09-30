ca34293412520f90f56c695d6b2cced32c50e78c
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-09-30 19:32:39 +0530

    dfs of graph

---
+++ Data Structures Problems/Graph/Easy/DFS_of_Graph/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void dfsUtils(vector<int> g[], vector<int> &dfsVec, unordered_set<int> &visited, int v){
+    if(g[v].size() == 0 or visited.count(v) == 1)
+        return;
+
+    dfsVec.emplace_back(v);
+    visited.emplace(v);
+
+    for(int new_v : g[v])
+        dfsUtils(g, dfsVec, visited, new_v);
+}
+
+vector <int> dfs(vector<int> g[], int N){
+    vector<int> dfsVec;
+
+    unordered_set<int> visited;
+    dfsUtils(g, dfsVec, visited, 0);
+
+    return dfsVec;
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+
+        int N, E;
+        cin>>N>>E;
+
+        vector<int> g[N];
+        bool vis[N];
+
+        memset(vis, false, sizeof(vis));
+
+        for(int i=0;i<E;i++)
+        {
+            int u,v;
+            cin>>u>>v;
+            g[u].push_back(v);
+            g[v].push_back(u);
+        }
+
+        vector <int> res = dfs(g, N);
+        for (int i = 0; i < res.size (); i++)
+            cout << res[i] << " ";
+        cout<<endl;
+
+    }
+}  // } Driver Code Ends
