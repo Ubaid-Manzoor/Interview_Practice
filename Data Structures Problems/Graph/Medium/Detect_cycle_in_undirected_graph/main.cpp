7d7990dd53de6d5636bb2dd58359a4da56e99629
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-10-03 10:25:33 +0530

    detect cycle using bfs

--- Data Structures Problems/Graph/Medium/Detect_cycle_in_undirected_graph/main.cpp
+++ Data Structures Problems/Graph/Medium/Detect_cycle_in_undirected_graph/main.cpp
@@ -29,12 +29,61 @@ bool dfs(vector<int> g[], unordered_set<int> &visited, int v, int parent){
     return false;
 }
 
+bool bfs(vector<int> g[],unordered_set<int> &visited, int v){
+    queue<pair<int,int>> q;
+    q.emplace(make_pair(v,-1));
+
+    while(not q.empty()){
+        pair<int,int> p = q.front();q.pop();
+        int current_vertex = p.first;
+        int parent_vertex = p.second;
+
+        visited.emplace(current_vertex);
+
+        for(int new_v: g[current_vertex]){
+            if(new_v == parent_vertex)
+                continue;
+            if(visited.count(new_v) == 1)
+                return true;
+
+            q.emplace(make_pair(new_v,current_vertex));
+        }
+    }
+
+    return false;
+}
+
+void printGraph(vector<int> g[], int V){
+    for(int i = 0 ; i < V ; i++){
+        cout<<i<<"->";
+        for(int vertex: g[i])
+            cout<<vertex<<"->";
+        cout<<endl;
+    }
+}
+
+
 bool isCyclic(vector<int> g[], int V){
     unordered_set<int> visited;
+    printGraph(g,V);
+
+//    // Using DFS
+//    for(int vertex = 0 ; vertex < V ; vertex++){
+//        if(visited.count(vertex) == 0){
+//            bool cycleExist = dfs(g, visited, vertex, -1);
+//            if(cycleExist)
+//                return true;
+//        }
+//    }
+//    return false;
+
+
+    // Using BFS
 
     for(int vertex = 0 ; vertex < V ; vertex++){
         if(visited.count(vertex) == 0){
-            bool cycleExist = dfs(g, visited, vertex, -1);
+//            cout<<"vertex :::: "<<vertex<<endl;
+            bool cycleExist = bfs(g, visited, vertex);
             if(cycleExist)
                 return true;
         }
@@ -61,7 +110,7 @@ int main()
         for(int i=0;i<E;i++)
         {
             cin>>u>>v;
-
+//            cout<<u<<"->"<<v<<endl;
             // adding edge to the graph
             adj[u].push_back(v);
             adj[v].push_back(u);
