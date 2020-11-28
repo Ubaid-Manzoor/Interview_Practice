1773f7c62f71e1dfa7ac3e75cf5469f5089779fd
Ubaid-Manzoor <ubaidmanzoor12@gmail.com>
2020-11-28 11:40:53 +0530

    product of 2 number using recursion

---
+++ Data Structures Problems/Recursion and Backtracking/Basic/product_of_two_number/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int product(int x, int y){
+    if(x < y)
+        swap(x, y);
+    else if(y != 0)
+        return x + product(x, y-1);
+    else
+        return 0;
+}
+
+// Driver Code
+int main()
+{
+    int x = 5, y = 2;
+    swap(x, y);
+    cout<<x<<" "<<y<<endl;
+    cout << product(x, y);
+    return 0;
+}
---
+++ Data Structures Problems/Recursion and Backtracking/Easy/binary_to_gray_code_using_recursion/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int binary_to_gray(int binary_number){
+
+}
+
+int main()
+{
+    int binary_number = 1011101;
+
+    printf("%d", binary_to_gray(binary_number));
+    return 0;
+}
